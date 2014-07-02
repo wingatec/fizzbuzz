58d2027b63bfbff8e0cdd857b94bd34265c8a47d
wingatec <carmelitawingate@gmail.com>
2014-07-02 19:42:53 -0400

    fizzbuzz

---
+++ fizzbuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            var list = Enumerable.Range(1, 100)
+                .Select(n =>
+                {
+                    if (n % 15 == 0)
+                    {
+                        return "FizzBuzz";
+                    }
+                    if (n % 3 == 0)
+                    {
+                        return "Fizz";
+                    }
+                    if (n % 5 == 0)
+                    {
+                        return "Buzz";
+                    }
+                    return n.ToString();
+                });
+
+            foreach (string item in list)
+                Console.WriteLine(item);
+            Console.ReadKey();
+
+
+        }
+    }
+}
